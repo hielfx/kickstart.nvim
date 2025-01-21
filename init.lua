f9135d42b27ded659250d6e17da566efeb49dd22
Daniel Sáchez <danielsn@ext.inditex.com>
2025-01-21 13:28:30 +0100

    feat: configure json formatting with prettierd

--- init.lua
+++ init.lua
@@ -661,6 +661,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettierd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -718,6 +719,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        json = { 'prettierd' },
       },
     },
   },
