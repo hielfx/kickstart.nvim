9810a4544650fa6a99c51ba755f46cff7081b158
Daniel Sáchez <danielsn@ext.inditex.com>
2025-09-08 14:16:46 +0200

    feat(formatter): format css using prettierd

--- init.lua
+++ init.lua
@@ -813,7 +813,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        -- css = { 'prettierd' },
+        css = { 'prettierd' },
         typescript = { 'prettierd' },
         typescriptreact = { 'prettierd' },
         json = { 'prettierd' },
